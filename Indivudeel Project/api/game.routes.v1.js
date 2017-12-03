088cb67bb2f6bd7dff170115497a10661a1f4b1a
Jcjmfrij <jcjmfrij@avans.nl>
2017-12-03 19:24:39 +0100

    App Server Toegevoegd

---
+++ Indivudeel Project/api/game.routes.v1.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const routes = express.Router();
+const mongodb = require('../config/mongo.db');
+const games = require('../model/game.model');
+
+routes.get('/games', function(req, res) {
+    res.contentType('application/json');
+    games.find({})
+        .then((games) => {
+        res.status(200).json({
+        'game': games
+    });
+})
+    .catch((error) => res.status(400).json(error));
+});
+
+
+routes.get('/games/:id', function(req, res) {
+    res.contentType('application/json');
+    const id = req.param('id');
+    console.log(id);
+    games.find({_id: id})
+        .then((games) => {
+        res.status(200).json({
+        'game': games
+    });
+})
+    .catch((error) => res.status(400).json(error));
+});
+
+
+routes.post('/games', function(req, res) {
+    const gameProps = req.body;
+
+    games.create(gameProps)
+        .then((games) => {
+        res.status(200).send(games)
+})
+    .catch((error) => res.status(400).json(error))
+});
+
+
+routes.put('/games/:id', function(req, res) {
+    res.contentType('application/json');
+    const gameId = req.params.id;
+    const gameProps = req.body;
+
+    games.findByIdAndUpdate({_id: gameId}, gameProps)
+        .then(()=> games.findById({_id: gameId}))
+    .then(driver => res.send(driver))
+    .catch((error) => res.status(400).json(error))
+
+});
+
+
+routes.delete('/games/:id', function(req, res) {
+    const id = req.param('id');
+    games.findByIdAndRemove(id)
+        .then((status) => res.status(200).json({
+        'status': 'game has been deleted',
+        'game': status
+    }))
+    .catch((error) => res.status(400).json(error))
+});
+
+module.exports = routes;
---
+++ Indivudeel Project/model/game.model.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CharacterSchema = require('./characters').CharacterSchema;
+const PlatformSchema = require('./platforms').PlatformSchema;
+
+const GameSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    characters: [CharacterSchema],
+    imagePath: String,
+    platforms: [CharacterSchema],
+    developer: {
+        type: String,
+        required: true
+    },
+    publisher: {
+        type: String,
+        required: true
+    }
+
+});
+
+
+const Game = mongoose.model('game', GameSchema);
+
+Game.count({}, function (err, count) {
+    if(count < 5){
+        console.log('add a game');
+        const game = new Game({
+            title: "Overwatch",
+            genre: "First-person shooter",
+            description: "In a time of global crisis, an international task force of heroes banded together to restore peace to a war-torn world: OVERWATCH. It ended the crisis and helped to maintain peace in the decades that followed, inspiring an era of exploration, innovation, and discovery. But after many years, Overwatch's influence waned, and it was eventually disbanded.",
+            characters: [
+                {
+                    name: 'Junkrat'
+                },
+                {
+                    name: 'Roadhog'
+                },
+                {
+                    name: 'Solder 76'
+                }
+            ],
+            imagePath: 'https://www.geforce.com/sites/default/files-world/featured_image/overwatch-keyart.jpg',
+            platforms: [
+                {
+                    name: 'Microsoft Windows'
+                },
+                {
+                    name: 'Playstation 4'
+                },
+                {
+                    name: 'Xbox One'
+                }
+            ],
+            developer: "Blizzard Entertainment",
+            publisher: "Blizzard Entertainment",
+        }).save();
+    }
+    else {
+        console.log('zit al een game in de db')
+    }
+});
+
+
+
+
+module.exports = Game;
